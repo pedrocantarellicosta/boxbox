f19ef82e4fec8d27d924bfb0217165efc47eed0c
Pedro Cantarelli Costa <pedrocantarellicosta@live.com>
2019-05-19 23:17:23 -0300

    Updates

---
+++ precache-manifest.db0150025aaeec887f11a2fd50dbf7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bbba81831bc566c21235faeb5d631a",
+    "url": "/boxbox/index.html"
+  },
+  {
+    "revision": "ceb462ced17d8b8086d8",
+    "url": "/boxbox/static/css/main.f69bbf9a.chunk.css"
+  },
+  {
+    "revision": "956718f34145aa477672",
+    "url": "/boxbox/static/js/2.76b1312b.chunk.js"
+  },
+  {
+    "revision": "ceb462ced17d8b8086d8",
+    "url": "/boxbox/static/js/main.0ccecf70.chunk.js"
+  },
+  {
+    "revision": "a4b5d806e229e39fc386",
+    "url": "/boxbox/static/js/runtime~main.4e2a3bd3.js"
+  },
+  {
+    "revision": "c22fd429e3618eec41599c0a9be625f9",
+    "url": "/boxbox/static/media/boxbox3.c22fd429.png"
+  }
+]);
